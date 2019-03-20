d83a6baeff9de8341be57dbbac3a7773fe103a42
Rafae Noor <abdurrafae98@live.com>
2019-03-20 23:35:25 +0500

    Basic body code

--- src/index.js
+++ src/index.js
@@ -19,102 +19,128 @@ var config = {
     update: update
   } 
 };
- 
+
 var game = new Phaser.Game(config);
- 
+
 function preload() {
-    this.load.image('ball', 'assets/ball.png');
+  this.load.image('ball', 'assets/ball.png');
 }
 
 function addPlayer(self, playerInfo) {
-    self.snake = self.physics.add.image(playerInfo.x, playerInfo.y, 'ball').setOrigin(0.5, 0.5);
+  self.snake = self.physics.add.image(playerInfo.x, playerInfo.y, 'ball').setOrigin(0.5, 0.5);
+  var snakeBody = playerInfo["body"];
+  self.body = playerInfo["body"];
+  self.snakeBody = snakeBody.map(pos => { return self.physics.add.image(pos.x, pos.y, 'ball').setOrigin(0.5,0.5); })
+
 }
 
 function addOther(self, playerInfo) {
-    const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'ball').setOrigin(0.5, 0.5);
-    otherPlayer.playerId = playerInfo.playerId;
-    self.otherPlayers.add(otherPlayer);
+  const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'ball').setOrigin(0.5, 0.5);
+  otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayers.add(otherPlayer);
 }
- 
+
 function create() {
 
-    //Enable cursor
-    this.cursors = this.input.keyboard.createCursorKeys();
+  //Enable cursor
+  this.cursors = this.input.keyboard.createCursorKeys();
 
-    var self = this;
-    this.socket = io();
-    this.otherPlayers = this.physics.add.group();
-    this.socket.on('renderGame', function (players) {
-        //iterate over the player info object from server
-        Object.keys(players).forEach(function (id) {
-            if (players[id].playerId === self.socket.id) {
-                addPlayer(self, players[id]);
-            } else {
-                addOther(self, players[id])
-            }
-        });
+  var self = this;
+  this.socket = io();
+  this.otherPlayers = this.physics.add.group();
+  this.socket.on('renderGame', function (players) {
+    //iterate over the player info object from server
+    Object.keys(players).forEach(function (id) {
+      if (players[id].playerId === self.socket.id) {
+        addPlayer(self, players[id]);
+      } else {
+        addOther(self, players[id])
+      }
     });
+  });
 
-    this.socket.on('newPlayer', function (playerInfo) {
-        addOther(self, playerInfo);
+  this.socket.on('newPlayer', function (playerInfo) {
+    addOther(self, playerInfo);
+  });
+
+
+  this.socket.on('playerMoved', function (playerInfo) {
+    console.log('movement recieved')
+    self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+      if (playerInfo.playerId === otherPlayer.playerId) {
+        otherPlayer.setRotation(playerInfo.rotation);
+        otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+      }
     });
+  });
 
-
-    this.socket.on('playerMoved', function (playerInfo) {
-        console.log('movement recieved')
-        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-            if (playerInfo.playerId === otherPlayer.playerId) {
-                otherPlayer.setRotation(playerInfo.rotation);
-                otherPlayer.setPosition(playerInfo.x, playerInfo.y);
-            }
-        });
+  this.socket.on('disconnect', function (playerId) {
+    self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+      if (playerId === otherPlayer.playerId) {
+        otherPlayer.destroy();
+      }
     });
-
-    this.socket.on('disconnect', function (playerId) {
-        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-            if (playerId === otherPlayer.playerId) {
-                otherPlayer.destroy();
-            }
-        });
-    });
+  });
 }
- 
+
+var counter = 0;
 function update() {
-    if(this.snake){
+  if(this.snake){
 
-        if (this.cursors.left.isDown) {
-            this.snake.setAngularVelocity(-150);
-        } else if (this.cursors.right.isDown) {
-            this.snake.setAngularVelocity(150);
-        } else{
-            this.snake.setAngularVelocity(0);
-        }
-
-        if (this.cursors.up.isDown) {
-            this.physics.velocityFromRotation(this.snake.rotation, 100, this.snake.body.velocity)
-        } else {
-            this.snake.body.velocity.setTo(0,0);
-        }
-    
-        this.physics.world.wrap(this.snake, 5);
-
-        var x = this.snake.x;
-        var y = this.snake.y;
-        var r = this.snake.rotation;
-
-        if (this.snake.oldPosition && (x !== this.snake.oldPosition.x || y !== this.snake.oldPosition.y || r !== this.snake.oldPosition.rotation)) {
-            console.log('movement emitted')
-            this.socket.emit('playerMovement', { x: this.snake.x, y: this.snake.y, rotation: this.snake.rotation });
-        }
-        // save old position data
-        this.snake.oldPosition = {
-            x: this.snake.x,
-            y: this.snake.y,
-            rotation: this.snake.rotation
-        };            
+    if (this.cursors.left.isDown) {
+      this.snake.setAngularVelocity(-150);
+      console.log('Turning left');
+    } else if (this.cursors.right.isDown) {
+      this.snake.setAngularVelocity(150);
+      console.log('Turning right');
+    } else{
+      this.snake.setAngularVelocity(0);
     }
 
-  
 
-    
+    /*
+    if (this.cursors.up.isDown) {
+      this.physics.velocityFromRotation(this.snake.rotation, 100, this.snake.body.velocity)
+    } else {
+      this.snake.body.velocity.setTo(0,0);
+    }
+    */
+    this.physics.velocityFromRotation(this.snake.rotation,100, this.snake.body.velocity);
+
+
+
+
+    this.physics.world.wrap(this.snake, 5);
+
+    var x = this.snake.x; 
+    var y = this.snake.y;
+    var r = this.snake.rotation;
+
+    if (this.snake.oldPosition && (x !== this.snake.oldPosition.x || y !== this.snake.oldPosition.y || r !== this.snake.oldPosition.rotation)) {
+      //console.log('movement emitted')
+      this.socket.emit('playerMovement', { x: this.snake.x, y: this.snake.y, rotation: this.snake.rotation });
+    }
+    // save old position data
+    this.snake.oldPosition = {
+      x: this.snake.x,
+      y: this.snake.y,
+      rotation: this.snake.rotation
+    };            
+    //this.snake.body.setPosition(this.snake.oldPosition.x, this.snake.oldPosition.y);
+
+    counter++;
+
+    if(counter % 15 == 0){
+      this.body.pop();
+      this.body.unshift({"x": this.snake.oldPosition.x , "y": this.snake.oldPosition.y});
+    }
+
+
+    for(let i = 0; i < this.body.length; i++){
+      this.snakeBody[i].x = this.body[i].x ;
+      this.snakeBody[i].y = this.body[i].y ;
+    }
+
+
+  }
 }
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ var PORT = 8000
 
 
 const server = http.createServer(async(req,resp) =>{
-	resp.end(await readFile(req.url.substr(1)))
+  resp.end(await readFile(req.url.substr(1)))
 })
 
 const readFile = f => new Promise((resolve,reject) =>
@@ -16,41 +16,50 @@ const io = socketio(server)
 
 var players = {}
 
+const chunks = 10;
+
 io.on('connection', socket =>{
-	console.log('A player connected')
-	
-	// create a new player and add it to our players object
-	players[socket.id] = {
-		rotation: 0,
-		x: Math.floor(Math.random() * 700) + 50,
-		y: Math.floor(Math.random() * 500) + 50,
-		playerId: socket.id,
-	};
-	
-	// send the info of players already playing to the new player
-	socket.emit('renderGame', players);
-	
-	// update all other players of the new player
-	socket.broadcast.emit('newPlayer', players[socket.id]);
+  console.log('A player connected')
 
-	socket.on('playerMovement', function (movementData) {
-		console.log('movement recieved')
-		players[socket.id].x = movementData.x;
-		players[socket.id].y = movementData.y;
-		players[socket.id].rotation = movementData.rotation;
-		// emit a message to all OTHER players about the player that moved
-		socket.broadcast.emit('playerMoved', players[socket.id]);
-	});
+  // create a new player and add it to our players object
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+  };
+
+  players[socket.id]["body"] = Array(10).fill(0).map((_,index) => {
+    return { "x": (-1*chunks*(index+1) + players[socket.id].x)  , "y": players[socket.id].y };
+  })
 
 
-	// when a player disconnects, remove them from our players object
-	socket.on('disconnect', function () {
-		console.log('user disconnected');
-		// remove this player from our players object
-		delete players[socket.id];
-		// emit a message to all players to remove this player
-		io.emit('disconnect', socket.id);
-	})
+
+
+  // send the info of players already playing to the new player
+  socket.emit('renderGame', players);
+
+  // update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+
+  socket.on('playerMovement', function (movementData) {
+    //console.log('movement recieved')
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    players[socket.id].rotation = movementData.rotation;
+    // emit a message to all OTHER players about the player that moved
+    socket.broadcast.emit('playerMoved', players[socket.id]);
+  });
+
+
+  // when a player disconnects, remove them from our players object
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+    // remove this player from our players object
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    io.emit('disconnect', socket.id);
+  })
 })
 
 
